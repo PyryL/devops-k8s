191015ee467dca54c1abc34d06e0b7e2624f8fb2
Pyry Lahtinen <pyry@pyry.info>
2024-07-03 12:12:09 +0300

    exercise 1.05

--- todo-project/index.js
+++ todo-project/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.type('html').send('<h1>Todo</h1><i>Under construction...</i>');
+});
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
