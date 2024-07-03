ef38df4f9194db826e4af6b29bfcf9c9ef834500
Pyry Lahtinen <pyry@pyry.info>
2024-07-03 12:53:26 +0300

    exercise 1.07

--- log-output/index.js
+++ log-output/index.js
@@ -1,6 +1,9 @@
+const express = require('express');
 const { v4: uuid } = require('uuid');
 
-let randomStr = uuid();
+const randomStr = uuid();
+
+const app = express();
 
 const logString = () => {
   console.log(new Date().toISOString(), randomStr)
@@ -8,3 +11,9 @@ const logString = () => {
 
 logString();
 setInterval(logString, 5000);
+
+app.get('/', (req, res) => {
+  res.type('text/plain').send(`${new Date().toISOString()}: ${randomStr}`);
+});
+
+app.listen(process.env.PORT || 8080);
