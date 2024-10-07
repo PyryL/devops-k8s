50ff2f28f8543178035780e6de2268e4997df8bb
Pyry Lahtinen <pyry@pyry.info>
2024-10-07 16:51:59 +0300

    move persistent volume file (ex 3.03)

--- todo-project/manage.sh
+++ todo-project/manage.sh
@@ -5,9 +5,9 @@ set -e
 if [ "$1" == "apply" ]; then
     kubectl apply -f ../misc/namespace_todo.yaml
     kubens todo
-    kubectl apply -f ../misc/todo_persistent_volume.yaml -f manifests/ -f backend/manifests/
+    kubectl apply -f manifests/ -f backend/manifests/
 elif [ "$1" == "delete" ]; then
-    kubectl delete -f ../misc/todo_persistent_volume.yaml -f manifests/ -f backend/manifests/
+    kubectl delete -f manifests/ -f backend/manifests/
 else
     echo "usage: $0 {apply|delete}"
     exit 1
